88472b1ce946950356bb3e3c35d45a4bc976efe6
Kachi <onyedikannaji7@gmail.com>
2025-03-20 16:15:04 +0100

    new

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ function generateTicket(name, email, github, avatarUrl) {
     populateTicketNumber();
 }
 
-function populateTicketNumber() {
-    const ticketNumber = `#${Math.floor(10000 + Math.random() * 90000)}`;
-    document.getElementById('ticketNumber').textContent = ticketNumber;
-}
+// function populateTicketNumber() {
+//     const ticketNumber = `#${Math.floor(10000 + Math.random() * 90000)}`;
+//     document.getElementById('ticketNumber').textContent = ticketNumber;
+// }
